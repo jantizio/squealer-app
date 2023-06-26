42771efe30dcca835bc7a32165f07061d4a62bee
jantizio <emanuele0246@gmail.com>
2023-06-26 12:41:38 +0200

    config file update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/*.{js,ts,jsx,tsx}'],
+  content: [
+    './index.html',
+    './src/*.{js,ts,jsx,tsx,css}',
+    './src/components/**/*.{js,ts,jsx,tsx,css}',
+  ],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+    },
   },
   plugins: [],
 };
