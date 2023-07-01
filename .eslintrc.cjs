cc4ce00a9813daa7dcc3a09c8a2e881869b64105
jantizio <emanuele0246@gmail.com>
2023-07-01 10:50:40 +0200

    new packages and config files

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,11 +5,23 @@ module.exports = {
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
     'plugin:jsx-a11y/recommended',
+    'plugin:eslint-plugin-react-refresh/recommended',
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   plugins: ['react-refresh', 'jsx-a11y'],
   rules: {
     'react-refresh/only-export-components': 'warn',
+  },
+  settings: {
+    'import/resolver': {
+      alias: {
+        extensions: ['.jsx', '.tsx', '.scss', '.d.ts'],
+        map: [
+          ['@/components', './src/components'],
+          ['@/hooks', './src/hooks'],
+        ],
+      },
+    },
   },
 };
