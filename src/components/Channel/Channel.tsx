09eb8330b7040899d8d59ef12fcd7f417d8ab645
jantizio <emanuele0246@gmail.com>
2023-08-17 09:42:38 +0200

    baseline for discover and channel page

--- src/components/Channel/Channel.tsx
+++ src/components/Channel/Channel.tsx
@@ -1,8 +1,44 @@
 import { useParams } from 'react-router-dom';
+import HeaderLogo from '@/components/HeaderLogo';
+import MessageScroller from '@/components/MessageScroller';
+import { post_t } from '@/lib/types';
 
 const Channel = () => {
   let { channelName } = useParams<{ channelName: string }>();
-  return <div>Canale {channelName}</div>;
+
+  const fetchChannelSqueal = async (page: number): Promise<post_t[]> => {
+    return [
+      {
+        id: 0,
+        title: 'Prova',
+        body: 'YOOOOOOOO',
+        username: 'Jimmy',
+      },
+    ];
+  };
+
+  return (
+    <>
+      <header className="w-full order-first flex items-center justify-around my-3">
+        <HeaderLogo />
+      </header>
+
+      <div className="flex overflow-hidden">
+        {/* Main content */}
+        <main className="w-full order-2 overflow-auto md:w-4/6 lg:w-1/2">
+          <h1>Canale {channelName}</h1>
+          <MessageScroller fetchPostPage={fetchChannelSqueal} />
+        </main>
+
+        {/* Left sidebar */}
+        <aside className="w-full hidden order-1 overflow-auto md:block md:w-2/6 lg:w-1/4"></aside>
+
+        {/* Right sidebar */}
+        <aside className="w-full hidden order-3 overflow-hidden lg:block lg:w-1/4"></aside>
+      </div>
+      <footer className="w-full order-last p-4"></footer>
+    </>
+  );
 };
 
 export default Channel;
--- src/components/Discover/Discover.tsx
+++ src/components/Discover/Discover.tsx
@@ -1,5 +1,38 @@
+import HeaderLogo from '@/components/HeaderLogo';
+import ChannelList from '@/components/ChannelList';
+import { channel_t } from '@/lib/types';
+import useAxios from '@/hooks/useAxios';
+
 const Discover = () => {
-  return <div>Scopri i canali</div>;
+  const privateAPI = useAxios();
+  const fetchEveryChannel = async () => {
+    const channelsApi: string = 'https://jsonplaceholder.typicode.com/albums';
+    const res = await privateAPI.get<channel_t[]>(channelsApi);
+    // const channels = await privateAPI.get<channel_t[]>('/channels/');
+    return res.data;
+  };
+
+  return (
+    <>
+      <header className="w-full order-first flex items-center justify-around my-3">
+        <HeaderLogo />
+      </header>
+
+      <div className="flex overflow-hidden">
+        {/* Main content */}
+        <main className="w-full order-2 overflow-auto md:w-4/6 lg:w-1/2">
+          <ChannelList fetchChannels={fetchEveryChannel} />
+        </main>
+
+        {/* Left sidebar */}
+        <aside className="w-full hidden order-1 overflow-auto md:block md:w-2/6 lg:w-1/4"></aside>
+
+        {/* Right sidebar */}
+        <aside className="w-full hidden order-3 overflow-hidden lg:block lg:w-1/4"></aside>
+      </div>
+      <footer className="w-full order-last p-4"></footer>
+    </>
+  );
 };
 
 export default Discover;
