128a5483c6fc41bb5b3bacd88008185761c05e81
jantizio <emanuele0246@gmail.com>
2023-09-14 09:17:29 +0200

    added utility components

---
+++ src/components/PersistLogin/PersistLogin.tsx
@@ -0,0 +1,41 @@
+import { Outlet } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import useRefreshToken from '@/hooks/auth/useRefreshToken';
+import useAuth from '@/hooks/auth/useAuth';
+
+const PersistLogin = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const refresh = useRefreshToken();
+  const { auth, persist } = useAuth();
+
+  useEffect(() => {
+    let isMounted = true;
+
+    const verifyRefreshToken = async () => {
+      try {
+        await refresh();
+      } catch (err) {
+        console.error(err);
+      } finally {
+        isMounted && setIsLoading(false);
+      }
+    };
+
+    !auth?.token && persist ? verifyRefreshToken() : setIsLoading(false);
+
+    return () => {
+      isMounted = false;
+    };
+  }, []);
+
+  useEffect(() => {
+    console.log(`isLoading: ${isLoading}`);
+    console.log(`aT: ${JSON.stringify(auth?.token)}`);
+  }, [isLoading]);
+
+  return (
+    <>{!persist ? <Outlet /> : isLoading ? <p>Loading...</p> : <Outlet />}</>
+  );
+};
+
+export default PersistLogin;
---
+++ src/components/RequireAuth/RequireAuth.tsx
@@ -0,0 +1,31 @@
+import { useLocation, Navigate, Outlet } from 'react-router-dom';
+import useAuth from '@/hooks/auth/useAuth';
+import { run } from '@/lib/utils';
+import { userType_t } from '@/lib/types';
+
+type RequireAuthProps = {
+  allowedRoles: userType_t[];
+  loginPath: string;
+  unauthorizedPath: string;
+};
+
+const RequireAuth = ({
+  allowedRoles,
+  loginPath,
+  unauthorizedPath,
+}: RequireAuthProps) => {
+  const { auth } = useAuth();
+  const location = useLocation();
+
+  return run(() => {
+    if (!auth)
+      return <Navigate to={loginPath} state={{ from: location }} replace />;
+    else if (allowedRoles.includes(auth.authState.type)) return <Outlet />;
+    else
+      return (
+        <Navigate to={unauthorizedPath} state={{ from: location }} replace />
+      );
+  });
+};
+
+export default RequireAuth;
