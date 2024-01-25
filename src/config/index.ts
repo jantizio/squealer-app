c817730de4becba7a1aaa9cb710cdc072c69a373
jantizio <emanuele0246@gmail.com>
2024-01-25 22:34:52 +0100

    feat: last TODO and version bump

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,7 +2,7 @@ import type { CookieAttributes } from 'node_modules/@types/js-cookie';
 
 // export const BASE_URL = 'https://site222315.tw.cs.unibo.it/api';
 export const BASE_URL = 'http://localhost:8000/api';
-export const nonTextQuota = 80; //TODO: it should be 125
+export const nonTextQuota = 125;
 export const tempSquealCookieKey = 'temporizedSqueals';
 export const cookieOptions: CookieAttributes = {
   expires: new Date(2147483647 * 1000), // arbitrarily large expiration date
