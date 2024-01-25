fde3b69eb23907e51423b28af3539f9a23df0391
jantizio <emanuele0246@gmail.com>
2024-01-25 22:43:58 +0100

    feat: cache clear after logout

--- src/hooks/useLogout.ts
+++ src/hooks/useLogout.ts
@@ -1,13 +1,16 @@
 import { useLogout as useLogoutLib } from '@/lib/auth';
+import { useQueryClient } from '@tanstack/react-query';
 import { useNavigate } from 'react-router-dom';
 import { toast } from 'sonner';
 
 export const useLogout = () => {
   const navigate = useNavigate();
+  const queryClient = useQueryClient();
 
   const { mutate, ...rest } = useLogoutLib({
     onSuccess() {
       toast.success('Logout effettuato con successo');
+      queryClient.clear();
       navigate('/');
     },
     meta: {
